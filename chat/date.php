8ad5eebe25e16db509cf68c28d7c62d8d75e1c02
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2025-02-23 13:44:40 -0500

    update this to be current

--- chat/date.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-session_start();
-
-$fp = fopen("log.html", 'a');
-
-fwrite($fp, "<div class='msgln' style='text-align:center;'><i>- - - - - ".date("l, jS \of F Y")." - - - - -</i><br><br></div>");
-fclose($fp);
-?>
